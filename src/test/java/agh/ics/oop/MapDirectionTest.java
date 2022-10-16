cafc204935a8b835fb166b25ebaa722b3f861863
kkkucia <11.karolinakucia@gmail.com>
2022-10-16 21:44:20 +0200

    Task2 repaired

--- src/test/java/agh/ics/oop/MapDirectionTest.java
+++ src/test/java/agh/ics/oop/MapDirectionTest.java
@@ -1,8 +1,8 @@
 package agh.ics.oop;
 
-import org.testng.annotations.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.testng.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class MapDirectionTest {
--- src/test/java/agh/ics/oop/Vector2dTest.java
+++ src/test/java/agh/ics/oop/Vector2dTest.java
@@ -1,8 +1,9 @@
 package agh.ics.oop;
 
-import org.testng.annotations.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.testng.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
+
 
 public class Vector2dTest {
     @Test
